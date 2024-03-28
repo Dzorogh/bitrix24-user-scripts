d53b0058afe47b7863ced7edf180551aad5339e4
Сергей Инденбом <dzorogh@gmail.com>
2024-03-28 11:11:07 +0300

    Create conver-to-input.js

---
+++ conver-to-input.js
@@ -0,0 +1,102 @@
+// ==UserScript==
+// @name         Bitrix24 Convert Input To Select
+// @namespace    https://crm.globaldrive.ru/
+// @version      2024-02-22-2
+// @description  Tasks doesn't have dropdown lists in our version of B24, this scripts solves this problem.
+// @author       Dzorogh
+// @match        https://crm.globaldrive.ru/*
+// @require      https://cdn.jsdelivr.net/npm/public-google-sheets-parser@latest
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    const lsKey = 'convertInputToSelectSheetId';
+
+    let sheetId = localStorage.getItem(lsKey);
+
+    if (!sheetId || sheetId === 'null') {
+        sheetId = prompt('Please enter Google Sheet Id');
+        if (sheetId) {
+            localStorage.setItem(lsKey, sheetId);
+        }
+    }
+
+    if (!sheetId) {
+        console.error('No sheet ID provided');
+        return false;
+    }
+    
+    async function convertInputToSelect(sheetId, sheetName, fieldId) {
+        const parser = new PublicGoogleSheetsParser(sheetId, { sheetName })
+        const parserData = await parser.parse();
+
+        console.log('convertInputToSelect: parserData', parserData)
+
+        if (!parserData.length) {
+            localStorage.removeItem(lsKey);
+        }
+
+        const options = parserData.map((item) => {
+            return item.value;
+        })
+
+        console.log('convertInputToSelect: options', options)
+
+        const defaultOption = parserData.find((item) => {
+            return item.is_default == 1;
+        })
+
+        console.log('convertInputToSelect: defaultOption', defaultOption)
+
+        const inputElement = document.querySelector(`input[name*='${fieldId}']`);
+
+        if (!inputElement) {
+            console.error(inputElement)
+            throw new Error('Element not found')
+        }
+
+        const copyAttsTo = (from, to) => {
+            const attrs = from.attributes;
+
+            for (const attr of attrs) {
+                if (attr.name !== 'size') {
+                    to.setAttribute(attr.name, attr.value);
+                }
+            }
+        }
+        
+
+        const newSelectElement = document.createElement("select");
+
+        copyAttsTo(inputElement, newSelectElement);
+
+        inputElement.replaceWith(newSelectElement);
+
+        newSelectElement.style.width = '100%';
+
+        options.unshift('')
+
+        options.forEach((option) => {
+            const optionElement = document.createElement('option');
+            optionElement.value = option;
+            optionElement.textContent = option;
+
+            if (inputElement.value === option) {
+                optionElement.selected = true;
+            } else if (!inputElement.value && defaultOption && defaultOption.value === option) {
+                optionElement.selected = true;
+            }
+
+            newSelectElement.append(optionElement);
+        })
+    }
+
+    convertInputToSelect(sheetId, 'Companies', 'UF_AUTO_555825536710');
+
+    convertInputToSelect(sheetId, 'Projects', 'UF_AUTO_231937255950');
+
+    convertInputToSelect(sheetId, 'Types', 'UF_AUTO_692505608773');
+
+    convertInputToSelect(sheetId, 'Priorities', 'UF_AUTO_367625648403');
+    
+})();
